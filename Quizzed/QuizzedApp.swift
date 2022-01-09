90379db435ff8d57a944cb95839142f80a044542
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2022-01-09 12:58:02 +0000

    Initial Commit

---
+++ Quizzed/QuizzedApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzedApp.swift
+//  Quizzed
+//
+//  Created by Aaron Johncock on 09/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizzedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
