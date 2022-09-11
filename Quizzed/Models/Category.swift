0d1e7b15f49e47d59f232030d307ce01d08b2ec2
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2022-09-11 15:44:19 +0100

    - Refactor HomeView and separate each section into it's own variable

--- Quizzed/Models/Category.swift
+++ Quizzed/Models/Category.swift
@@ -13,18 +13,3 @@ struct Category: Identifiable {
     let image: String
     let categoryID: String
 }
-
-var categories = [
-
-    Category(category: Categories.animals, image: Categories.animals, categoryID: "27"),
-    Category(category: Categories.comics, image: Categories.comics, categoryID: "29"),
-    Category(category: Categories.film, image: Categories.film, categoryID: "11"),
-    Category(category: Categories.generalKnowledge, image: Categories.generalKnowledge, categoryID: "9"),
-    Category(category: Categories.music, image: Categories.music, categoryID: "12"),
-    Category(category: Categories.mythology, image: Categories.mythology, categoryID: "20"),
-    Category(category: Categories.television, image: Categories.television, categoryID: "14"),
-    Category(category: Categories.scienceAndNature, image: Categories.scienceAndNature, categoryID: "17"),
-    Category(category: Categories.sport, image: Categories.sport, categoryID: "21"),
-    Category(category: Categories.videoGames, image: Categories.videoGames, categoryID: "15")
-
-]
--- Quizzed/Views/HomeView.swift
+++ Quizzed/Views/HomeView.swift
@@ -11,10 +11,8 @@ struct HomeView: View {
     
     @EnvironmentObject var quizModel: QuizViewModel
 
-    
     @State private var selectedCategory = ""
     @State private var categoryImage = ""
-
     
     @State private var showRules = false
     @State var showStats = false
@@ -27,149 +25,24 @@ struct HomeView: View {
     
     var body: some View {
         
-
             NavigationView {
                     VStack(spacing: 0) {
                         
-                        // MARK: Title Bar
-                      
-                            HStack {
-                                
-                                Button {
-                                    withAnimation {
-                                        showStats.toggle()
-                                    }
-                                } label: {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 5)
-                                            .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
-                                        
-                                        Image(systemName: "chart.bar.fill")
-                                            .font(.title3).dynamicTypeSize(.medium)
-                                            .foregroundColor(Color("Blue3"))
-                                            .accessibilityLabel("Statistics")
-                                        
-                                    }
-                                    .frame(width: 32, height: 32)
-                                }
-                                
-                                Spacer()
-                                
-                                Text("QUIZZED")
-                                    .font(.largeTitle.bold())
-                                    .tracking(5)
-                                    .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
-                                
-                                Spacer()
-                                
-                                Button {
-                                    withAnimation {
-                                        showRules.toggle()
-                                    }
-                                } label: {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 5)
-                                            .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
-                                        
-                                        
-                                        Text("?")
-                                            .font(.title.bold()).dynamicTypeSize(.medium)
-                                            .foregroundColor(Color("Blue3"))
-                                            .accessibilityLabel("Help and rules")
-                                        
-                                    }
-                                    .frame(width: 32, height: 32)
-                                }
-                            }
-                            .padding(.horizontal)
-                            .padding(.top, ScreenOptions.headerPadding())
-                            .padding(.bottom, 25)
-                            .background(
-                                ZStack {
-                                RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 25)
-                                    .foregroundColor(Color("Red"))
-                                    
-                                RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 25)
-                                    .stroke(Color("Red2").opacity(0.5), lineWidth: 20)
-                                    .blur(radius: 20)
-                                }
-                            )
-                            .clipShape(RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 25))
+                        // MARK: Header
+                           header
                                                 
                         // MARK: Select Category
                         Text("Select a category")
                             .font(.title2)
                             .padding(.top, 20)
                         
-                        
                         // MARK: Categories
-                        ScrollView(showsIndicators: false) {
-                            
-                            
-                            LazyVGrid(columns: columns, spacing: 15) {
-                                ForEach(categories, id: \.id) { item in
-                                    
-                                    Button {
-                                        if selectedCategory == item.category {
-                                            withAnimation {
-                                                selectedCategory = ""
-                                            }
-                                        } else {
-                                            withAnimation {
-                                                selectedCategory = item.category
-                                            }
-                                        }
-                                        quizModel.categoryID = item.categoryID
-                                        quizModel.selectedCategory = item.category
-                                        print(selectedCategory)
-                                    } label: {
-                                        CategoryButton(selectedCategory: $selectedCategory, category: item.category)
-
-                                    }
-
-                                }
-                            }
-                            .padding(.vertical)
-                            
-                        }
-                        .frame(maxWidth: .infinity)
-                        .frame(maxHeight: ScreenOptions.isScreenLarge ? 600 : 430)
-                        .padding(.horizontal)
-                        .padding(.top, 10)
-                        
+                        categoriesContent
                         
                         Spacer()
 
                         // MARK: Next Button
-                        
-                        NavigationLink {
-                            DifficultyView()
-
-                        } label: {
-
-                            Text("NEXT")
-                                .font(.largeTitle)
-                                .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
-                                .frame(maxWidth: .infinity)
-                                .padding(.bottom, ScreenOptions.bottomButtonPadding())
-                                .padding(.top, 15)
-                                .background(
-                                    ZStack {
-                                        RoundedCornerShape(corners: [.topLeft, .topRight], radius: 25)
-                                            .foregroundColor(Color("Red"))
-                                        
-                                        RoundedCornerShape(corners: [.topLeft, .topRight], radius: 25)
-                                            .stroke(Color("Red2").opacity(0.5), lineWidth: 20)
-                                            .blur(radius: 20)
-                                    }
-                                )
-                                .clipShape(RoundedCornerShape(corners: [.topLeft, .topRight], radius: 25))
-                                .opacity(selectedCategory != "" ? 1 : 0.4)
-                            
-                        }
-                        .disabled(selectedCategory != "" ? false : true)
-
-                        
+                        nextButton
                         
                     }
                     .ignoresSafeArea()
@@ -189,6 +62,135 @@ struct HomeView: View {
             }
 
         
+    }
+    
+    var header: some View {
+        HStack {
+            
+            Button {
+                withAnimation {
+                    showStats.toggle()
+                }
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
+                    
+                    Image(systemName: "chart.bar.fill")
+                        .font(.title3).dynamicTypeSize(.medium)
+                        .foregroundColor(Color("Blue3"))
+                        .accessibilityLabel("Statistics")
+                    
+                }
+                .frame(width: 32, height: 32)
+            }
+            
+            Spacer()
+            
+            Text("QUIZZED")
+                .font(.largeTitle.bold())
+                .tracking(5)
+                .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
+            
+            Spacer()
+            
+            Button {
+                withAnimation {
+                    showRules.toggle()
+                }
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
+                    
+                    
+                    Text("?")
+                        .font(.title.bold()).dynamicTypeSize(.medium)
+                        .foregroundColor(Color("Blue3"))
+                        .accessibilityLabel("Help and rules")
+                    
+                }
+                .frame(width: 32, height: 32)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top, ScreenOptions.headerPadding())
+        .padding(.bottom, 25)
+        .background(
+            ZStack {
+            RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 25)
+                .foregroundColor(Color("Red"))
+                
+            RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 25)
+                .stroke(Color("Red2").opacity(0.5), lineWidth: 20)
+                .blur(radius: 20)
+            }
+        )
+        .clipShape(RoundedCornerShape(corners: [.bottomLeft, .bottomRight], radius: 25))
+    }
+    
+    var categoriesContent: some View {
+        ScrollView(showsIndicators: false) {
+            
+            LazyVGrid(columns: columns, spacing: 15) {
+                ForEach(Categories.allCategories, id: \.id) { item in
+                    
+                    Button {
+                        if selectedCategory == item.category {
+                            withAnimation {
+                                selectedCategory = ""
+                            }
+                        } else {
+                            withAnimation {
+                                selectedCategory = item.category
+                            }
+                        }
+                        quizModel.categoryID = item.categoryID
+                        quizModel.selectedCategory = item.category
+                        print(selectedCategory)
+                    } label: {
+                        CategoryButton(selectedCategory: $selectedCategory, category: item.category)
+
+                    }
+
+                }
+            }
+            .padding(.vertical)
+            
+        }
+        .frame(maxWidth: .infinity)
+        .frame(maxHeight: ScreenOptions.isScreenLarge ? 600 : 430)
+        .padding(.horizontal)
+        .padding(.top, 10)
+    }
+    
+    var nextButton: some View {
+        NavigationLink {
+            DifficultyView()
+
+        } label: {
+
+            Text("NEXT")
+                .font(.largeTitle)
+                .shadow(color: Color("Blue3"), radius: 3, x: 3, y: 3)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, ScreenOptions.bottomButtonPadding())
+                .padding(.top, 15)
+                .background(
+                    ZStack {
+                        RoundedCornerShape(corners: [.topLeft, .topRight], radius: 25)
+                            .foregroundColor(Color("Red"))
+                        
+                        RoundedCornerShape(corners: [.topLeft, .topRight], radius: 25)
+                            .stroke(Color("Red2").opacity(0.5), lineWidth: 20)
+                            .blur(radius: 20)
+                    }
+                )
+                .clipShape(RoundedCornerShape(corners: [.topLeft, .topRight], radius: 25))
+                .opacity(selectedCategory != "" ? 1 : 0.4)
+            
+        }
+        .disabled(selectedCategory != "" ? false : true)
     }
     
 }
